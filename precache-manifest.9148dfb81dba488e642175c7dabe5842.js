ced16ca3a31d6124b07e53ae06960fd2a9ad7b1f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-22 01:52:56 +0000

    Deploy FlyLikeSoarin/Fullstack-mipt-frontend to github.com/FlyLikeSoarin/Fullstack-mipt-frontend.git:gh-pages

---
+++ precache-manifest.9148dfb81dba488e642175c7dabe5842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1886f4755fc4cb92b0a095f23c47c33d",
+    "url": "/Fullstack-mipt-frontend/index.html"
+  },
+  {
+    "revision": "c43e57957e96f11342e5",
+    "url": "/Fullstack-mipt-frontend/static/css/main.fc8e34b4.chunk.css"
+  },
+  {
+    "revision": "fa16b0801b72109bce98",
+    "url": "/Fullstack-mipt-frontend/static/js/2.8b96946e.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/Fullstack-mipt-frontend/static/js/2.8b96946e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c43e57957e96f11342e5",
+    "url": "/Fullstack-mipt-frontend/static/js/main.0c66bca8.chunk.js"
+  },
+  {
+    "revision": "7b737933937b7ae446e0",
+    "url": "/Fullstack-mipt-frontend/static/js/runtime-main.46e14984.js"
+  }
+]);
